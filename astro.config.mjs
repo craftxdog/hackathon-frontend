dfb0d568e908305454820695463b19a0fec79dae
AaronUlloa <aaronmars77@gmail.com>
2023-09-13 03:10:23 -0600

    Frontend-Workspace

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    scripts: {
+        entry: './dist/js/bundle.min.js', // Usamos el archivo minificado
+        type: 'module',
+        map: { url: 'bundle.js.min.map' } // Ruta al mapa de origen
+    }
+});
