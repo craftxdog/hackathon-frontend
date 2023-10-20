8da653f77f8fa861ea329ca299aefa2125bcd3da
Remy349 <santoraga15@gmail.com>
2023-10-19 19:26:16 -0600

    Added new path for the admin dashboard.
    Basic setup.

--- src/routes/iniciarSesion.tsx
+++ src/routes/iniciarSesion.tsx
@@ -1,9 +1,9 @@
 export const IniciarSesion = () => {
-  return <>
-    <h1>INICIAR SESION - VISTA PARA EL INICIO DE SESION!</h1><h1>asasasasas</h1>
-    <nav>
-      asadasdasdasdasd
-      asdasdasdasdasd
-    </nav>
-  </> 
+  return (
+    <>
+      <h1>INICIAR SESION - VISTA PARA EL INICIO DE SESION!</h1>
+      <h1>asasasasas</h1>
+      <nav>asadasdasdasdasd asdasdasdasdasd</nav>
+    </>
+  )
 }
